83563bdd7e5f5f677fb910994b63e6f9d672e655
16IMA <imonfort001@gmail.com>
2025-12-04 21:35:08 +0100

    feat: creación componente TS para tarjeta de artista

--- src/components/ArtistaCard.tsx
+++ src/components/ArtistaCard.tsx
@@ -1,2 +1,15 @@
+interface Artista {
+    nombre: string;
+    estilo: string;
+    seguidores: number;
+    web?: string;
+}
 
+interface ArtuistaProps {
+    artista: Artista;
+    onUpdate: (datos: Partial<Artista>) => void;
+}
 
+const ArtistaCard: React.FC<ArtistaProps> = ({ artista, onUpdate }) => {
+    
+}
