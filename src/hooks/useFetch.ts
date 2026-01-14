7f48907d12e7c00a4a60699e36d852a882cd44b5
16IMA <imonfort001@gmail.com>
2026-01-14 21:52:09 +0100

    create fetch

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+
+export const useEffect = <T>(url: string) => {
+    const [data, setData] = useState<T | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
+    
+}
