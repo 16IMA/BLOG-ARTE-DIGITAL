31cf5ad8b284ea64b35b497a8006c0ff6213ca62
16IMA <imonfort001@gmail.com>
2026-01-17 21:50:28 +0100

    feat: create fetch TS

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,9 +1,32 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
-export const useEffect = <T>(url: string) => {
+export const useFetch = <T> (url: string) => {
+
     const [data, setData] = useState<T | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
 
+    useEffect(() => {
+        const fetchData = async () => {
+            try { 
+                setLoading(true);
+                const response = await fetch(url);
+                 if (!response.ok) throw new Error('Error en la red');
+
+                 const result: T = await response.json();
+
+                 setData(result);
+        } catch (err: any) {
+            setError(err.message);
+        } finally {
+            setLoading(false);
+        }
+        };
+
+        fetchData();
+    }, [url]);
+
+    return { data, loading, error };
+};
+
     
-}
