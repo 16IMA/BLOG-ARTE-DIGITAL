0bac3fc2c5cb9bb813ad10eb7e36345925dae85f
16IMA <imonfort001@gmail.com>
2026-01-27 22:35:22 +0100

    feat: change artistcard to artpostcard

---
+++ src/components/ArtPostCard.tsx
@@ -0,0 +1,30 @@
+interface ArtPost {
+    título: string;
+    subtitulo: string;
+    cuerpo: number;
+    fecha: string;
+}
+
+interface ArtPostProps {
+    post: ArtPost;
+    onSave?: (post: ArtPost) => void;
+}
+
+const ArtPostCard: React.FC<ArtPostProps> = ({ post, onSave }) => {
+    return (
+        <article className={styles.article}>
+            <header>
+                <h2 className={styles.title}>{post.título}</h2>
+                <p className={styles.subtitle}>{post.subtitulo}</p>
+            </header>
+
+            <div className={styles.body}>
+                {post.cuerpo}
+            </div>
+
+            {onSave && <button onClick={() => onSave(post)}>Pub</button>
+        </article>
+    );
+};
+
+export default ArtistaCard;
