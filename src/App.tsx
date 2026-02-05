56662fd7dd6113d4de1a74b18bb23993185b6d96
16IMA <imonfort001@gmail.com>
2026-02-05 20:04:43 +0100

    feat: create header components and finished conect with gemine API

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,8 @@ function App() {
 
      try{
         const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
-        const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+        const model = genAI.getGenerativeModel(
+            { model: "gemini-3-flash-preview" });
 
         const prompt = `Eres una experta en arte digital. Escribe una crítica corta y profesional sobre: ${ideaSemilla}. Devuelve el resultado con este formato: Título: [título] Cuerpo: [contenido]`;
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+<header className="header-container">
+    <div className="header-wrapper">
+        <img src="/public/logo-IMA.png" alt="Logo IMA" className="header-logo" />
+    </div>
+
+    <div>
+        <img src="/public/menu-hamburguesa.png" alt="Menú" className="menu-icon" />
+    </div>
+</header>
