00ac946b7a1a8979f366551fc3cb7cd1bea1a25f
16IMA <imonfort001@gmail.com>
2026-02-16 19:08:30 +0100

    feat: footer css done

---
+++ src/pages/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import ArtPostCard, { type ArtPost } from "../components/ArtPostCard";
+import EditorAsistente from "../components/EditorAsistente";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+
+function App() {
+  
+}
+
+
+
+export default App;
