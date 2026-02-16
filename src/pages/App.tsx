1a3de29a6e44039ba0f006176705e9d723256815
16IMA <imonfort001@gmail.com>
2026-02-16 21:46:25 +0100

    feat: start routing

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,7 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import BlogPanel from "../components/BlogPanel";
 
 function App() {
   
