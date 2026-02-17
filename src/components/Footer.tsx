53af7bd0bf233f8ee081812656f9b6c315731244
16IMA <imonfort001@gmail.com>
2026-02-17 21:34:46 +0100

    feat: routing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import { FaInstagram, FaTiktok } from "react-icons/fa";
 
+const Footer = () => {
 <footer className="footer-container">
     <div className="footer-menu">
         <a href="#">Inicio</a>
@@ -11,6 +12,15 @@ import { FaInstagram, FaTiktok } from "react-icons/fa";
         <a href="https://www.instagram.com/arte.digital.blog/" target="_blank" rel="noopener noreferrer">
             <FaInstagram size={24} />
         </a>
-        
+        <a href="https://www.tiktok.com/@arte.digital.blog" target="_blank" rel="noopener noreferrer">
+            <FaTiktok size={24} />
+        </a>
+    </div>
+
+    <div className="footer-copyright">
+        <p>&copy; {new Date().getFullYear()} Arte Digital Blog. Todos los derechos reservados.</p>
     </div>
 </footer>
+};
+
+export default Footer;
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,10 +1,28 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Footer  from "../components/Footer";
 import BlogPanel from "../components/BlogPanel";
+import HomePage from '../pages/HomePage';
+import AboutPage from '../pages/AboutPage';
 
 function App() {
-  
+  return 
+  (
+    <BrowserRouter>
+        <div className="app-container">
+            <Header />
+            <main className="main-content">
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/about" element={<AboutPage />} />
+                    {/* Ruta privada */}
+                    <Route path="/admin-panel" element={<BlogPanel />} />
+                </Routes>
+            </main>
+            <Footer />
+        </div>
+    <BrowserRouter/>
+  );
 }
 
 
