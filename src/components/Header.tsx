701eac08ccc9e4f57f9b9c4987f12dae4cd7e2f5
16IMA <imonfort001@gmail.com>
2026-02-27 21:27:24 +0100

    feat: change header and footer styles, erase index css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,13 +4,15 @@ const Header = () => {
 
     return(
         <header className={styles.headerContainer}>
+            
+            <div className={styles.headerMenuContainer}>
+                <img src="/menu-hamburguesa.png" alt="Menú" className={styles.menuIcon} />
+            </div>
+
             <div className={styles.headerLogoContainer}>
                 <img src="/logo-IMA.png" alt="Logo IMA" className={styles.headerLogo} />
             </div>
 
-            <div className={styles.headerMenuContainer}>
-                <img src="/menu-hamburguesa.png" alt="Menú" className={styles.menuIcon} />
-            </div>
         </header>
     );
 };
