b2fe5b29770e6badfb110f4bc45a91726d0d314f
16IMA <imonfort001@gmail.com>
2026-08-18 16:49:05 +0200

    feat: run build

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/inovation_media_art/',
+  base:'/innovation_media_art/',
   plugins: [
     react(),
     tailwindcss()
